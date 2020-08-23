b95aa6fa17b7d7d0f1a4e2c26fcdc269ac5cc59b
Kemal Ahmed <goatandsheep@gmail.com>
2020-08-23 17:37:15 -0400

    init demo commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function tempCheck(evt) {
+    console.log(evt)
+}
+
+/**
+ * Listener for closed captions video events
+ * @param {String} vidId 
+ * @param {Function} showCallback callback when cue is shown
+ * @param {Function} hideCallback callback when cue is hidden
+ */
+function closedCaptionsListener(vidEl, showCallback, hideCallback) {
+    const tracks = vidEl.videoTracks
+    for (let i = 0, len = tracks.length; i < len; i++) {
+        i.addEventListener('cuechange', tempCheck)
+    }
+}
